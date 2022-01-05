0dec10464467290fa0bb90a7c10061128f3cb4a6
thomasAriza <thom.ariza@yahoo.fr>
2022-01-05 20:46:13 +0100

    last commit

---
+++ untitled0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  5 16:02:46 2022
+
+@author: thoma
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
